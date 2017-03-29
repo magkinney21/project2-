75b4e282c81367ed48d9635709a62573f968b143
Magkinney21 <magkinney21@gmail.com>
2017-03-29 13:39:34 -0400

    done

--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,19 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/stars_db');
+// mongoose.connect('mongodb://localhost/stars_db');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/stars_db');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 mongoose.promise = global.Promise;
 
 var User = require('../models/user');
--- server.js
+++ server.js
@@ -18,7 +18,21 @@ var app = express();
 // ADD THE NAME OF YOUR DATABASE
 // mongoose.connect('mongodb://localhost/<YourDatabaseNameHere>');
 
-mongoose.connect('mongodb://localhost/stars_db');
+// mongoose.connect('mongodb://localhost/stars_db');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/stars_db');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 var db = mongoose.connection;
 
 app.set('view engine', 'hbs')
